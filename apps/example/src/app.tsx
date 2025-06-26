23802bcd3735d7e3dd5b1fe3e6944e21c5e612c3
Lucas Augusto Barreto Linhares <lablinhares.develcode@rd.com.br>
2025-06-26 12:07:21 -0300

    compile react package

---
+++ apps/example/src/app.tsx
@@ -0,0 +1,15 @@
+import * as Button from "@repo/react/v2/components/button"
+import * as Header from '@repo/react/v2/blocks/header'
+
+export function App() {
+  return (
+    <div>
+      <Header.Root />
+
+      <Button.Root>
+        Button
+      </Button.Root>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
